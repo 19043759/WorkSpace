5ca9906a6848fa550749029007d3b0d281e9cf39
Nathan Foglia <19043759@saultcollege.ca>
2020-01-26 16:38:52 -0500

    Chapter 2 hands on

---
+++ calc/Styles/calc.js
@@ -0,0 +1,9 @@
+function clear(){
+    var calcClear = 0
+    document.getElementsById("clear").addEventListener("click", calcClear, false);
+}
+
+function number(){
+    var display = 0
+    document.getElementsByClassName("num").addEventListener("click", display.append("display"))
+}
