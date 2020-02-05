25e2365af4392aac7b0ddad36829f31a06c26093
Nathan Foglia <nfoglia@lssu.edu>
2020-02-04 21:28:26 -0800

    New calc code

--- calc/js/calc.js
+++ calc/js/calc.js
@@ -1,9 +1,20 @@
-    function clearCalc(){
-        document.getElementById("display").innerHTML = 0
-    }
+function clearCalc(){
+    document.getElementById("display").innerHTML = 0
+}
 
-    function number(number){
-        var curNum = document.getElementById("display").innerHTML;
+function number(number){
+    var curNum = document.getElementById("display").innerHTML;
+    if (curNum == 0) {
+        document.getElementById("display").innerHTML = number
+    }
+    else{
         document.getElementById("display").innerHTML = curNum + number
     }
 
+}
+function decimal() {
+    var addDecimal = document.getElementById("display").innerHTML;
+    var isDecimal = addDecimal.indexOf(".");
+    if (isDecimal != true) 
+        document.getElementById("display").innerHTML = addDecimal + "."
+}
